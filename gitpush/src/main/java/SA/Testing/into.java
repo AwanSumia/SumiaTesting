7c4f86fb879f2a78b51ba0459f1f5b60a12e8e02
awansumia <sumia.awan530@gmail.com>
2023-12-17 17:50:17 +0500

    my first commit

---
+++ gitpush/src/main/java/SA/Testing/into.java
@@ -0,0 +1,23 @@
+package SA.Testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class into {
+    
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver;
+driver=new FirefoxDriver();
+//System.setProperty("gekoDriver", "C:\\Users\\hclou\\Downloads\\geckodriver-v0.33.0-win-aarch64\\.exe");
+driver.get("https://www.google.com");
+
+	}
+	
+	
+	
+
+}
---
+++ gitpush/src/main/java/SA/Testing/testng.java
@@ -0,0 +1,15 @@
+package SA.Testing;
+
+import org.testng.annotations.Test;
+
+public class testng {
+    
+	@Test
+	public void testng()
+	{
+	  System.out.println("TestNG");
+	}
+	
+	
+	
+}
